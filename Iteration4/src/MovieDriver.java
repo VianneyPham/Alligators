2fe3f425201945d3d33d2761f98305ea91af9e34
VianneyPham <gd3451er@go.minnstate.edu>
2021-03-31 16:42:05 -0500

    Update MovieDriver.java

--- Iteration4/src/MovieDriver.java
+++ Iteration4/src/MovieDriver.java
@@ -12,45 +12,146 @@ public class MovieDriver {
 		Connection allConn = null;
 		try{ 	
 			allConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", "root", "");
+			
+			Statement allStmt = allConn.createStatement();
 
-			PreparedStatement mstmt = allConn.prepareStatement("INSERT INTO movies(native_name, year_made) SELECT DISTINCT native_name, year_made FROM ms_test_data WHERE NOT EXISTS (SELECT 1 FROM movies WHERE native_name = ms_test_data.native_name and year_made = ms_test_data.year_made)");		
-			int movieCreated = mstmt.executeUpdate();
+			ResultSet rs = allStmt.executeQuery("SELECT * FROM ms_test_data");
 
-			PreparedStatement sstmt = allConn.prepareStatement("INSERT INTO songs(title) SELECT DISTINCT title FROM ms_test_data WHERE NOT EXISTS ( SELECT title FROM songs WHERE songs.title = ms_test_data.title)");		
-			int songCreated = sstmt.executeUpdate();			
+			String movieRS = "SELECT * FROM movies WHERE native_name = ? AND year_made = ?";
 
-			PreparedStatement msStmt = allConn.prepareStatement("INSERT INTO movie_song (movie_id, song_id) SELECT movies.movie_id, songs.song_id FROM movies, songs WHERE NOT EXISTS (SELECT 1 FROM movie_song WHERE movies.movie_id = movie_song.movie_id )");
-			int msCreated = msStmt.executeUpdate();
+			String insertMovie = "INSERT INTO movies (native_name, year_made, english_name) VALUES (?,?,?)";
 
+			String songRS = "SELECT title FROM songs WHERE title = ?";
 
-			if(movieCreated > 1 && songCreated > 1 && msCreated > 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Created MS Created' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated < 1 && songCreated < 1 && msCreated < 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Ignored MS Ignored' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated > 1 && songCreated < 1 && msCreated < 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Ignored MS Ignored' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated < 1 && songCreated > 1 && msCreated < 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Created MS Ignored' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated < 1 && songCreated < 1 && msCreated > 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Ignored MS Created' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated > 1 && songCreated > 1 && msCreated < 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Created MS Ignored' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated > 1 && songCreated < 1 && msCreated > 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Ignored MS Created' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
-			}else if(movieCreated < 1 && songCreated > 1 && msCreated > 1){
-				PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Created MS Created' WHERE ID = ms_test_data.ID");
-				movieStatusUpdateM.execute();
+			String insertSong = "INSERT INTO songs (title,theme) VALUES (?,?)";
+
+			PreparedStatement pmaxid = allConn.prepareStatement("SELECT max(ID) FROM ms_test_data");
+			ResultSet maxIDrs = pmaxid.executeQuery();
+			int maxID = 0;
+			if( maxIDrs.next()){
+				maxID = maxIDrs.getInt(1);
 			}
+
+			for (int i = 0; i < maxID; i++){
+				
+				rs.next();
+				int movieCreated = 0;
+				int songCreated = 0;
+				int msCreated = 0;
+
+				int currentMovie = 0;
+
+				String nativeNameTD = rs.getString("native_name");
+				int yearMadeTD = rs.getInt("year_made");
+			
+				PreparedStatement ps = allConn.prepareStatement(movieRS);
+				ps.setString(1, nativeNameTD);
+				ps.setInt(2,yearMadeTD);
+
+				ResultSet mrs = ps.executeQuery();
+				System.out.println(mrs.next());
+				mrs.beforeFirst();
+				if (mrs.next() == true)
+				{
+					System.out.println("Movie already exists!");
+					currentMovie += 1;
+				}
+				else
+				{
+					PreparedStatement moviePS = allConn.prepareStatement(insertMovie);
+					moviePS.setString(1,nativeNameTD);
+					moviePS.setInt(2,yearMadeTD);
+					moviePS.setString(3,nativeNameTD);
+
+					int row = moviePS.executeUpdate();
+					System.out.println("Movie has been added!");
+					movieCreated += 1;
+					currentMovie += 1;
+				}
+
+				String titleTD = rs.getString("title");
+				titleTD = titleTD.replace("\"","");
+				System.out.println(titleTD);
+				PreparedStatement song_ps = allConn.prepareStatement(songRS);
+				song_ps.setString(1, titleTD);
+
+				ResultSet srs = song_ps.executeQuery();
+				System.out.println(srs.next());
+
+				srs.beforeFirst();
+				if (srs.next() == true)
+				{
+					System.out.println("Song already exists!");
+				}
+				else
+				{
+					PreparedStatement songPS = allConn.prepareStatement(insertSong);
+					songPS.setString(1,titleTD);
+					songPS.setString(2, "Null");
+
+					int row = songPS.executeUpdate();
+					System.out.println("Song has been added!");
+					songCreated += 1;
+				}
+
+				System.out.println(movieCreated);
+				System.out.println(songCreated);
+				System.out.println(msCreated);
+
+				if(movieCreated > 0 && songCreated > 0 && msCreated > 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Created MS Created' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				} 
+				else {
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Ignored MS Ignored' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+
+				if (movieCreated > 0 && songCreated == 0 && msCreated == 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Ignored MS Ignored' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+				
+				else if(movieCreated == 0 && songCreated > 0 && msCreated == 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Created MS Ignored' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+
+				else if(movieCreated == 0 && songCreated == 0 && msCreated > 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Ignored MS Created' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+
+				else if(movieCreated > 0 && songCreated > 0 && msCreated == 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Created MS Ignored' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+
+				else if(movieCreated > 0 && songCreated == 0 && msCreated > 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Created S Ignored MS Created' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+
+				else if(movieCreated == 0 && songCreated > 0 && msCreated > 0){
+					PreparedStatement movieStatusUpdateM = allConn.prepareStatement("UPDATE ms_test_data SET execution_status = 'M Ignored S Created MS Created' WHERE title = ?");
+					movieStatusUpdateM.setString(1,titleTD);
+					movieStatusUpdateM.execute();
+				}
+
+				
+				System.out.println("\n");
+			}
+
 		allConn.close();
-		
 		}
+
 		catch(Exception ex){
 			ex.printStackTrace();
 		}
